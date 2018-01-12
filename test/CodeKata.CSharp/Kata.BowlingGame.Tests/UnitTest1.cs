d498fd528d5ed3ddb0daab395ef3b3bf3d9edacc
Jobair Khan <m.j.khan.bd@gmail.com>
2018-01-12 19:17:41 +0000

    3 test

--- test/CodeKata.CSharp/Kata.BowlingGame.Tests/UnitTest1.cs
+++ test/CodeKata.CSharp/Kata.BowlingGame.Tests/UnitTest1.cs
@@ -3,12 +3,73 @@ using Xunit;
 
 namespace Kata.BowlingGame.Tests
 {
-    public class UnitTest1
+    public class BowlingGameTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly Game _game;
 
+        public BowlingGameTest1()
+        {
+            _game = GetSut();
+        }
+
+        private void RollMany(int count, int pins)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+
+        private static Game GetSut()
+        {
+            return new Game();
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.Equal(0, _game.Score());
+        }
+
+        [Fact]
+        public void TestAllOne()
+        {
+            RollMany(20, 1);
+
+            Assert.Equal(20, _game.Score());
+        }
+
+        //[Fact]
+        //public void TestOneSpare()
+        //{
+        //    _game.Roll(5);
+        //    _game.Roll(5);
+        //    _game.Roll(3);
+
+        //    RollMany(17, 0);
+
+        //    Assert.Equal(16, _game.Score());
+        //}
+    }
+
+    public class Game
+    {
+        private int _score = 0;
+        private int[] rolls = new int[21];
+        private int _currentRoll = 0;
+
+
+        public void Roll(int pins)
+        {
+            _score += pins;
+            rolls[_currentRoll++] = pins;
+        }
+
+        public int Score()
+        {
+            return _score;
         }
     }
 }
