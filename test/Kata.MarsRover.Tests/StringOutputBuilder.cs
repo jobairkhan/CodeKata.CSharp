cb9874b7a7f727a6cb6658d6a5c2bc7b6ceada58
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 18:58:24 +0000

    GREEN: Add new line

--- test/Kata.MarsRover.Tests/StringOutputBuilder.cs
+++ test/Kata.MarsRover.Tests/StringOutputBuilder.cs
@@ -1,30 +1,29 @@
 using System;
 using FluentAssertions;
 using Xunit;
-namespace Kata.MarsRover.Tests
-{
-    public class StringOutputBuilder : IOutputBuilder
-    {
+namespace Kata.MarsRover.Tests {
+    public class StringOutputBuilder : IOutputBuilder {
         public string Result { get; private set; }
 
-        public void AddResult(string result)
-        {
-            Result = result;
+        public void AddResult(string result) {
+            if (string.IsNullOrWhiteSpace(Result)) {
+                Result = string.Empty;
+            }
+            else { Result += Environment.NewLine; }
+            Result += result;
         }
     }
 
     public class StringOutputBuilderShould {
         [Fact]
-        public void Add_to_result()
-        {
+        public void Add_to_result() {
             var outputBuilder = new StringOutputBuilder();
             outputBuilder.AddResult("Test");
             outputBuilder.Result.Should().Be("Test");
         }
-        
+
         [Fact]
-        public void Return_all_when_multiple_result_separated_by_new_line()
-        {
+        public void Return_all_when_multiple_result_separated_by_new_line() {
             var outputBuilder = new StringOutputBuilder();
             outputBuilder.AddResult("Test");
             outputBuilder.AddResult("Test");
