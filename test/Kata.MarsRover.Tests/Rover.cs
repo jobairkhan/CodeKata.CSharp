8bd1f69cbb4c5e3bd8ca70578b559b14a15931a3
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 11:38:11 +0000

    Test more cases

--- test/Kata.MarsRover.Tests/Rover.cs
+++ test/Kata.MarsRover.Tests/Rover.cs
@@ -1,17 +1,14 @@
 using System;
 using FluentAssertions;
 using Xunit;
-namespace Kata.MarsRover.Tests
-{
-    public class Rover
-    {
+namespace Kata.MarsRover.Tests {
+    public class Rover {
         private readonly Grid _grid;
         private Position _coordinates;
         private Compass _facing;
 
 
-        public Rover(Grid grid, Position coordinates, Compass facing)
-        {
+        public Rover(Grid grid, Position coordinates, Compass facing) {
             _grid = grid;
             _coordinates = coordinates;
             _facing = facing;
@@ -19,51 +16,52 @@ namespace Kata.MarsRover.Tests
 
         public string CurrentLocation => $"{_coordinates.X} {_coordinates.Y} {_facing}";
 
-        public void Go(char command)
-        {
-            if(command == 'R')
-            {
-                _facing = _facing switch
-                {
+        public void Go(char command) {
+            if (command == 'R') {
+                _facing = _facing switch {
                     Compass.N => Compass.E,
-                    Compass.S => Compass.W,
                     Compass.E => Compass.S,
+                    Compass.S => Compass.W,
+                    Compass.W => Compass.N,
                     _ => _facing
                 };
             }
-            else if(command == 'L')
-            {
+            else if (command == 'L') {
                 _facing = _facing switch {
                     Compass.N => Compass.W,
+                    Compass.W => Compass.S,
+                    Compass.S => Compass.E,
+                    Compass.E => Compass.N,
                     _ => _facing
                 };
             }
         }
     }
 
-    public class RoverShould
-    {
+    public class RoverShould {
         [Theory]
         [InlineData(Compass.N, "0 0 E")]
         [InlineData(Compass.E, "0 0 S")]
         [InlineData(Compass.S, "0 0 W")]
-        public void Go_right_when_command_is_R(Compass init, string expected)
-        {
-            var rover = new Rover(new Grid(5, 5), 
-                                  new Position(0, 0), 
+        [InlineData(Compass.W, "0 0 N")]
+        public void Go_right_when_command_is_R(Compass init, string expected) {
+            var rover = new Rover(new Grid(5, 5),
+                                  new Position(0, 0),
                                   init);
             rover.Go('R');
             rover.CurrentLocation
                 .Should()
                 .Be(expected);
         }
-        
+
         [Theory]
         [InlineData(Compass.N, "0 0 W")]
-        public void Go_left_when_command_is_L(Compass init, string expected)
-        {
-            var rover = new Rover(new Grid(5, 5), 
-                                  new Position(0, 0), 
+        [InlineData(Compass.W, "0 0 S")]
+        [InlineData(Compass.S, "0 0 E")]
+        [InlineData(Compass.E, "0 0 N")]
+        public void Go_left_when_command_is_L(Compass init, string expected) {
+            var rover = new Rover(new Grid(5, 5),
+                                  new Position(0, 0),
                                   init);
             rover.Go('L');
             rover.CurrentLocation
