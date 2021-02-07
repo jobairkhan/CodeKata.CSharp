2e4a70b700a602bc8c2f88068a402940f45690c7
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 00:26:29 +0000

    GREEN: PositionX

--- test/Kata.MarsRover.Tests/InputParser.cs
+++ test/Kata.MarsRover.Tests/InputParser.cs
@@ -4,62 +4,64 @@ using FluentAssertions;
 using Xunit;
 using static System.Int32;
 
-namespace Kata.MarsRover.Tests
-{
+namespace Kata.MarsRover.Tests {
     public class InputParser : IInputParser {
-        public (Grid, Position) Parse(string inputString)
-        {
-            var size = inputString.Split(" ");
+        public (Grid, Position) Parse(string inputString) {
+            var lines = inputString.Split(Environment.NewLine);
+            var size = lines[0].Split(" ");
             TryParse(size[0], out var height);
             TryParse(size[1], out var width);
-            return (new Grid{Height = height, Width = width }, new Position());
+            
+            
+            var position = lines[1].Split(" ");
+            TryParse(position[0], out var positionX);
+            return (new Grid { Height = height, Width = width }, new Position { X = positionX });
         }
     }
 
-    public class Position
-    {
+    public class Position {
         public int X { get; set; }
     }
 
-    public class Grid
-    {
+    public class Grid {
         public int Height { get; set; }
         public int Width { get; set; }
     }
 
-    public class InputParserShould
-    {
-        [Theory]
-        [InlineData("10 5", 10)]
-        [InlineData("5 5", 5)]
-        public void Return_grid_with_correct_height(string input, int expectedHeight)
-        {
+    public class InputParserShould {
+
+        [Theory, MemberData(nameof(GetInputVerifyHeight))]
+        public void Return_grid_with_correct_height(string input, int expectedHeight) {
             var inputParser = new InputParser();
             var (grid, _) = inputParser.Parse(input);
             grid.Height.Should().Be(expectedHeight);
         }
 
-        [Theory]
-        [InlineData("1 1", 1)]
-        [InlineData("10 10", 10)]
-        public void Return_grid_with_correct_width(string input, int expectedWidth)
-        {
+        [Theory, MemberData(nameof(GetInputVerifyWidth))]
+        public void Return_grid_with_correct_width(string input, int expectedWidth) {
             var inputParser = new InputParser();
             var (grid, _) = inputParser.Parse(input);
             grid.Width.Should().Be(expectedWidth);
         }
 
-        [Theory, MemberData(nameof(GetInput))]
-        public void Return_grid_with_correct_position(string input, int expectedX)
-        {
+        [Theory, MemberData(nameof(GetInputVerifyPositionX))]
+        public void Return_grid_with_correct_position(string input, int expectedX) {
             var inputParser = new InputParser();
             var (_, initialPosition) = inputParser.Parse(input);
             initialPosition.X.Should().Be(expectedX);
         }
 
-        public static IEnumerable<object[]> GetInput()
-        {
-            yield return new object[] {"1 1" + Environment.NewLine + "1 0 N", 1};
+        public static IEnumerable<object[]> GetInputVerifyHeight() {
+            yield return new object[] { "5 5" + Environment.NewLine + "1 0 N", 5 };
+            yield return new object[] { "10 10" + Environment.NewLine + "1 0 N", 10 };
+        }
+        public static IEnumerable<object[]> GetInputVerifyWidth() {
+            yield return new object[] { "1 1" + Environment.NewLine + "1 0 N", 1 };
+            yield return new object[] { "1 10" + Environment.NewLine + "1 0 N", 10 };
+        }
+        public static IEnumerable<object[]> GetInputVerifyPositionX() {
+            yield return new object[] { "1 1" + Environment.NewLine + "1 0 N", 1 };
+            yield return new object[] { "1 1" + Environment.NewLine + "10 0 N", 10 };
         }
     }
 }
