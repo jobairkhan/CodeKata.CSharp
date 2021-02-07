c9bfbb67114465c36f8e74b2f6b6946eb0723c72
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 02:41:34 +0000

    GREEN: RoverBuilder

--- test/Kata.MarsRover.Tests/Rover.cs
+++ test/Kata.MarsRover.Tests/Rover.cs
@@ -2,5 +2,18 @@ namespace Kata.MarsRover.Tests
 {
     public class Rover
     {
+        private readonly Grid _grid;
+        private readonly Position _coordinates;
+        private readonly Compass _facing;
+
+
+        public Rover(Grid grid, Position coordinates, Compass facing)
+        {
+            _grid = grid;
+            _coordinates = coordinates;
+            _facing = facing;
+        }
+
+        public string CurrentLocation => $"{_coordinates.X} {_coordinates.Y} {_facing}";
     }
 }
--- test/Kata.MarsRover.Tests/RoverBuilder.cs
+++ test/Kata.MarsRover.Tests/RoverBuilder.cs
@@ -5,39 +5,72 @@ using Xunit;
 namespace Kata.MarsRover.Tests
 {
     public class RoverBuilder : IBuildRover {
-        public IBuildRover WithGrid(Grid grid) {
-            throw new NotImplementedException();
+        private readonly Grid _grid;
+        private readonly Position _position;
+        private readonly Compass _compass;
+
+        public RoverBuilder()
+        {
+            _grid = new Grid(0, 0);
+        }
+
+        private RoverBuilder(Grid grid, Position position, Compass compass)
+        {
+            _grid = grid;
+            _position = position;
+            _compass = compass;
+        }
+
+        public IBuildRover WithGrid(Grid grid)
+        {
+            return new RoverBuilder(grid, _position, _compass);
         }
 
         public IBuildRover WithPosition(Position position)
         {
-            throw new NotImplementedException();
+            return new RoverBuilder(_grid, position, _compass);
         }
 
-        public IBuildRover WithFacing(Compass compass)
-        {
-            throw new NotImplementedException();
+        public IBuildRover WithFacing(Compass compass) {
+            return new RoverBuilder(_grid, _position, compass);
         }
 
         public Rover Build()
         {
-            return null;
+            return new Rover(_grid, _position, _compass);
         }
     }
 
     public class RoverBuilderShould
     {
-        private RoverBuilder _sut;
+        private readonly RoverBuilder _sut;
+        private readonly Rover _rover;
 
         public RoverBuilderShould()
         {
             _sut = new RoverBuilder();
+            _rover = _sut
+                .WithGrid(new Grid(10, 10))
+                .WithFacing(Compass.N)
+                .WithPosition(new Position(1, 1))
+                .Build();
         }
 
         [Fact]
         public void Not_return_nothing()
         {
-            _sut.Build().Should().NotBeNull();
+            _rover              
+                .Should()
+                .NotBeNull();
+        }
+    
+        [Fact]
+        public void Set_initial_location()
+        {
+            _rover
+                .CurrentLocation
+                .Should()
+                .Be("1 1 N");
         }
     }
 }
