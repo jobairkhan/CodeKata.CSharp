3d2cbbb2e656f5d2a976eda25b446c395fa3e9a1
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 02:54:35 +0000

    RED: When facing South and Go Right

--- test/Kata.MarsRover.Tests/Rover.cs
+++ test/Kata.MarsRover.Tests/Rover.cs
@@ -1,10 +1,13 @@
+using System;
+using FluentAssertions;
+using Xunit;
 namespace Kata.MarsRover.Tests
 {
     public class Rover
     {
         private readonly Grid _grid;
-        private readonly Position _coordinates;
-        private readonly Compass _facing;
+        private Position _coordinates;
+        private Compass _facing;
 
 
         public Rover(Grid grid, Position coordinates, Compass facing)
@@ -15,5 +18,51 @@ namespace Kata.MarsRover.Tests
         }
 
         public string CurrentLocation => $"{_coordinates.X} {_coordinates.Y} {_facing}";
+
+        public void Go(string command)
+        {
+            if(_facing == Compass.N) _facing = Compass.E;
+            else
+            {
+                _facing = Compass.S;
+            }
+        }
+    }
+
+    public class RoverShould
+    {
+        [Fact]
+        public void Go_right_when_command_R()
+        {
+            var rover = new Rover(new Grid(5, 5), new Position(0, 0), Compass.N);
+            rover.Go("R");
+            rover.CurrentLocation
+                .Should()
+                .Be("0 0 E");
+        }
+
+        [Fact]
+        public void Go_right_when_command_R_and_facing_E()
+        {
+            var rover = new Rover(new Grid(5, 5), 
+                                 new Position(0, 0), 
+                                 Compass.E);
+            rover.Go("R");
+            rover.CurrentLocation
+                .Should()
+                .Be("0 0 S");
+        }
+
+        [Fact]
+        public void Go_right_when_command_R_and_facing_S()
+        {
+            var rover = new Rover(new Grid(5, 5), 
+                                 new Position(0, 0), 
+                                 Compass.S);
+            rover.Go("R");
+            rover.CurrentLocation
+                .Should()
+                .Be("0 0 W");
+        }
     }
 }
