6063daa3f092d369f20765be273548ec6b384827
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 00:42:37 +0000

    REFACTOR: remove duplicate data

--- test/Kata.MarsRover.Tests/InputParser.cs
+++ test/Kata.MarsRover.Tests/InputParser.cs
@@ -20,9 +20,9 @@ namespace Kata.MarsRover.Tests {
         }
     }
 
-    public record Position(int x, int y);
+    public record Position(int X, int Y);
 
-    public record Grid(int height, int width);
+    public record Grid(int Height, int Width);
 
     public class InputParserShould {
 
@@ -30,47 +30,59 @@ namespace Kata.MarsRover.Tests {
         public void Return_grid_with_correct_height(string input, int expectedHeight) {
             var inputParser = new InputParser();
             var (grid, _) = inputParser.Parse(input);
-            grid.height.Should().Be(expectedHeight);
+            grid.Height.Should().Be(expectedHeight);
         }
 
         [Theory, MemberData(nameof(GetInputVerifyWidth))]
         public void Return_grid_with_correct_width(string input, int expectedWidth) {
             var inputParser = new InputParser();
             var (grid, _) = inputParser.Parse(input);
-            grid.width.Should().Be(expectedWidth);
+            grid.Width.Should().Be(expectedWidth);
         }
 
         [Theory, MemberData(nameof(GetInputVerifyPositionX))]
         public void Return_grid_with_correct_position_x(string input, int expectedX) {
             var inputParser = new InputParser();
             var (_, initialPosition) = inputParser.Parse(input);
-            initialPosition.x.Should().Be(expectedX);
+            initialPosition.X.Should().Be(expectedX);
         }
 
         [Theory, MemberData(nameof(GetInputVerifyPositionY))]
         public void Return_grid_with_correct_position_y(string input, int expectedY) {
             var inputParser = new InputParser();
             var (_, initialPosition) = inputParser.Parse(input);
-            initialPosition.y.Should().Be(expectedY);
+            initialPosition.Y.Should().Be(expectedY);
         }
 
+        private static string AllOne => "1 1" + Environment.NewLine + "1 1 N";
+        private static string AllFive => "5 5" + Environment.NewLine + "5 5 N";
+        private static string AllTen => "10 10" + Environment.NewLine + "10 10 N";
+        private static string Random => "1 5" + Environment.NewLine + "0 0 N";
         public static IEnumerable<object[]> GetInputVerifyHeight() {
-            yield return new object[] { "5 5" + Environment.NewLine + "1 0 N", 5 };
-            yield return new object[] { "10 10" + Environment.NewLine + "1 0 N", 10 };
+            yield return new object[] { AllOne, 1 };
+            yield return new object[] { AllFive, 5 };
+            yield return new object[] { AllTen, 10 };
+            yield return new object[] { Random, 1 };
         }
         public static IEnumerable<object[]> GetInputVerifyWidth() {
-            yield return new object[] { "1 1" + Environment.NewLine + "1 0 N", 1 };
-            yield return new object[] { "1 10" + Environment.NewLine + "1 0 N", 10 };
+            yield return new object[] { AllOne, 1 };
+            yield return new object[] { AllFive, 5 };
+            yield return new object[] { AllTen, 10 };
+            yield return new object[] { Random, 5 };
         }
 
         public static IEnumerable<object[]> GetInputVerifyPositionX() {
-            yield return new object[] { "1 1" + Environment.NewLine + "1 0 N", 1 };
-            yield return new object[] { "1 1" + Environment.NewLine + "10 0 N", 10 };
+            yield return new object[] { AllOne, 1 };
+            yield return new object[] { AllFive, 5 };
+            yield return new object[] { AllTen, 10 };
+            yield return new object[] { Random, 0 };
         }
 
         public static IEnumerable<object[]> GetInputVerifyPositionY() {
-            yield return new object[] { "1 1" + Environment.NewLine + "0 1 N", 1 };
-            yield return new object[] { "1 1" + Environment.NewLine + "0 10 N", 10 };
+            yield return new object[] { AllOne, 1 };
+            yield return new object[] { AllFive, 5 };
+            yield return new object[] { AllTen, 10 };
+            yield return new object[] { Random, 0 };
         }
     }
 }
