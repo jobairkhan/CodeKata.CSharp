ab4433825cf3e0a960e5cfef0938707fbd924875
Jobair Khan <jobair.khan@peachvideo.com>
2021-02-07 00:02:12 +0000

    GREEN: return correct width

--- test/Kata.MarsRover.Tests/InputParser.cs
+++ test/Kata.MarsRover.Tests/InputParser.cs
@@ -1,6 +1,7 @@
 using System;
 using FluentAssertions;
 using Xunit;
+using static System.Int32;
 
 namespace Kata.MarsRover.Tests
 {
@@ -8,14 +9,16 @@ namespace Kata.MarsRover.Tests
         public Grid Parse(string inputString)
         {
             var size = inputString.Split(" ");
-            int.TryParse(size[0], out var height);
-            return new Grid{Height = height };
+            TryParse(size[0], out var height);
+            TryParse(size[1], out var width);
+            return new Grid{Height = height, Width = width };
         }
     }
 
     public class Grid
     {
         public int Height { get; set; }
+        public int Width { get; set; }
     }
 
     public class InputParserShould
@@ -23,11 +26,21 @@ namespace Kata.MarsRover.Tests
         [Theory]
         [InlineData("10 5", 10)]
         [InlineData("5 5", 5)]
-        public void Return_grid_height(string input, int expectedHeight)
+        public void Return_grid_with_correct_height(string input, int expectedHeight)
         {
             var inputParser = new InputParser();
             var grid = inputParser.Parse(input);
             grid.Height.Should().Be(expectedHeight);
+        }
+
+        [Theory]
+        [InlineData("1 1", 1)]
+        [InlineData("10 10", 10)]
+        public void Return_grid_with_correct_width(string input, int expectedWidth)
+        {
+            var inputParser = new InputParser();
+            var grid = inputParser.Parse(input);
+            grid.Width.Should().Be(expectedWidth);
         }
     }
 }
